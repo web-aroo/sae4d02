78f0fd7dee3f48d02d28b90baf7d9787e2190e17
zina <zina.boufalaas@uha.fr>
2023-03-24 13:52:27 +0000

    ✨ Add Splash Screen useless fixup! ✨ Add Splash Screen ✨ Add Splash Screen
    Co-authored-by: Leon Lozahic <jb@leonekmi.fr>
    
    Merge-request: MAIN-MR-3
    Merged-by: Zina Boufalaas <zina.boufalaas@uha.fr>

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,24 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA(), vue()],
+  plugins: [VitePWA(
+      {
+        manifest: {
+          name: 'Mulhouse Mysteries',
+          start_url: '/',
+          display: 'standalone',
+          theme_color: '#271D15',
+          icons: [
+            {
+              src: 'src/assets/logo.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ],
+          background_color: '#271D15',
+        }
+      }
+  ), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
