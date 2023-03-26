451d00cfb67c4f35b3f16b68f49cd13979227395
davidismeurt <david.ismeurt@protonmail.com>
2023-03-26 17:27:44 +0200

    Fixed Dimmer + TalkingMouse

--- src/components/Dimmer.vue
+++ src/components/Dimmer.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="fixed inset-0 bg-black bg-opacity-50 z-50 grid place-items-center">
+  <div class="fixed inset-0 bg-black bg-opacity-80 z-50 grid place-items-center">
     <slot></slot>
   </div>
 
--- src/components/TalkingMouse.vue
+++ src/components/TalkingMouse.vue
@@ -3,7 +3,7 @@
   <Dimmer @click="next">
     <div class="relative">
       <img :src='"/img/mouses/"+character+".png"' alt="">
-      <div class="absolute bg-gray-50 p-4 bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 min-w-[32em] rounded">
+      <div class="absolute bg-gray-50 p-4 bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 min-w-[32em] rounded text-black">
         {{typewritedMsg}}
         <div class="absolute left-4 top-0 -translate-y-1/2 px-4 py-1 bg-light-brown text-white font-bold rounded">
           {{names[character]}}
