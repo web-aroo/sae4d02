1bb1d6504c625d23e9cca85613c3628f6c14b24c
davidismeurt <david.ismeurt@protonmail.com>
2023-03-26 15:15:37 +0200

    Added height to Container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,6 +1,6 @@
 <template>
 
-	<div class='max-w-xl mx-auto bg-dark-brown p-6 border-2 border-light-brown'>
+	<div class='max-w-xl mx-auto bg-dark-brown p-6 border-2 border-light-brown h-screen'>
 		<slot></slot>
 	</div>
 
