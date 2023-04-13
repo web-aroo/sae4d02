d953046b3478a74d8e7669db28e0e773d6005a29
leonekmi <me@leonekmi.fr>
2023-04-13 16:33:52 +0200

    patch pwa config

--- src/i18n.js
+++ src/i18n.js
@@ -8,7 +8,7 @@ i18next
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: import.meta.env.DEV,
     fallbackLng: 'en',
     resources: {
       en: {
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA(
-      {
-        manifest: {
-          name: 'Mulhouse Mysteries',
-          start_url: '/',
-          display: 'standalone',
-          theme_color: '#271D15',
-          icons: [
-            {
-              src: 'src/assets/logo.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ],
-          background_color: '#271D15',
-        }
-      }
-  ), vue()],
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: 'Mulhouse Mysteries',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#271D15',
+        icons: [
+          {
+            src: './src/assets/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        background_color: '#271D15'
+      },
+      includeAssets: ['img/**', 'sound/**', 'video/*']
+    }),
+    vue()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
