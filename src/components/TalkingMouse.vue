cdbff15907fdcdcafe1502e9ee628416fb5dba51
david <david.ismeurt@protonmail.com>
2023-04-13 14:31:33 +0200

    :sparkles: click

--- src/components/TalkingMouse.vue
+++ src/components/TalkingMouse.vue
@@ -3,13 +3,17 @@
     <div class="relative">
       <img :src="'/img/mouses/' + character + '.png'" alt="" />
       <div
-        class="absolute bg-gray-50 p-4 bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 min-w-[32em] min-h-[10rem] rounded text-black"
+        class="absolute bg-gray-50 p-4 bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 min-w-[24em] min-h-[10rem] rounded text-black"
       >
         {{ typewritedMsg }}
         <div
           class="absolute left-4 top-0 -translate-y-1/2 px-4 py-1 bg-light-brown text-white font-bold rounded"
         >
           {{ names[character] }}
+        </div>
+
+        <div class="Click w-8 absolute right-2 bottom-2">
+            <img src="/img/click.svg">
         </div>
       </div>
     </div>
@@ -58,3 +62,17 @@ function next() {
   typewritedMsg.value = props.message;
 }
 </script>
+
+<style scoped lang="scss">
+
+  .Click{
+    animation: bounce infinite ease-in-out 2s;
+    transform: rotate(-30deg);
+  }
+  
+  @keyframes bounce {
+    50%{
+      transform: translateY(-0.5em) rotate(-30deg);
+    }
+  }
+</style>
