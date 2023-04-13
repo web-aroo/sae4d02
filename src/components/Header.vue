b0ba9a9b4f50ccf707469be890056beb22d0fc98
david <david.ismeurt@protonmail.com>
2023-04-13 14:41:31 +0200

    :sparkles: back button working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 
   <header class="flex flex-row-reverse items-center justify-between">
     <LanguageSwitcher></LanguageSwitcher>
-    <BackButton v-if="backButton" @click="Sound.button()"></BackButton>
+    <BackButton v-if="backButton" @click="back()"></BackButton>
   </header>
 
 </template>
@@ -11,9 +11,17 @@
 import LanguageSwitcher from "@/components/LanguageSwitcher.vue";
 import BackButton from "@/components/BackButton.vue";
 import Sound from "@/utils/sound";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
 defineProps({
   backButton: Boolean
 });
+
+function back(){
+    Sound.button();
+    router.push("/games")
+}
 
 </script>
